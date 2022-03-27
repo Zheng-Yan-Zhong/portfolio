c4780b6634907fef839b38a6d76b3763615a31e7
Ian-Zheng <ococo09000@gmail.com>
2022-03-27 20:22:28 +0800

    2022/03/27

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,20 +1,26 @@
-import Skill from '../components/Skill'
-import Contact from '../components/Contact'
-import ProjectList from '../components/ProjectList'
-import { Content } from '../styles/Content'
-import Profile from '../components/Profile'
-import { Link } from 'react-router-dom'
-import { CgDetailsMore } from 'react-icons/cg'
+import Skill from '../components/Skill';
+import Contact from '../components/Contact';
+import ProjectList from '../components/ProjectList';
+import { Content } from '../styles/Content';
+import Profile from '../components/Profile';
+import { Link } from 'react-router-dom';
+import { CgDetailsMore } from 'react-icons/cg';
 function Home() {
   return (
     <Content>
       <Profile />
-      <Link style={{fontSize: "1rem", color: "lightgrey"}} to="/about"><CgDetailsMore />查看完整資料...</Link>
-      <Skill />   
+      <Link
+        style={{ fontSize: '1rem', color: 'lightgrey' }}
+        to="/myportfolio/about"
+      >
+        <CgDetailsMore />
+        查看完整資料...
+      </Link>
+      <Skill />
       <ProjectList />
       <Contact />
-  </Content>
-  )
+    </Content>
+  );
 }
 
-export default Home
+export default Home;
